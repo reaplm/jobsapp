8f1f806f194265fd1676f14e485f95eba345699b
PMolefe <PMolefe@G69BLAPICT80>
2020-12-03 09:02:08 +0200

    initial commit

---
+++ JobsApp/JobsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using JobsApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace JobsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
